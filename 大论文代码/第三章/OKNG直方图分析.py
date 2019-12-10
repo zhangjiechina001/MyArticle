4f7a3e70aa517d02c46714cbf5887266df2c069b
Administrator <295737015@qq.com>
2019-12-10 19:03:13 +0800

    gw504

--- 大论文代码/第三章/OKNG直方图分析.py
+++ 大论文代码/第三章/OKNG直方图分析.py
@@ -28,14 +28,34 @@ def dealTime(fun,**kwargs):
     ret=[imgData,totalMs*1000]
     return ret
 
-def drawHist(img):
+def drawHist(imgInfoList):
     #encoding:utf-8
-    src = cv2.imread('test01.jpg')
-    cv2.imshow("src", src)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    i=0
+    for imgInfo in imgInfoList:
+        img_name=imgInfo[0]
+        img_data=imgInfo[1]
+        i=i+1
+        plt.subplot(2,3,i)
+        thresh, mask_img = cv2.threshold(src=img_data, thresh=73, maxval=255, type=cv2.THRESH_BINARY)
+        hsit = cv2.calcHist([img_data], [0], mask_img, [256], [0, 255])
+        plt.plot(hsit,color='r')
+        cv2.imshow(img_name,mask_img)
+        plt.title("{0}".format(img_name), fontsize=20)
 
-    plt.hist(src.ravel(), 256)
     plt.show()
 
-src=cv2.imread('')
+ok_src1=cv2.imread('OKPictures//16_24_38.jpg')
+ok_src2=cv2.imread('OKPictures//16_50_50.jpg')
+ok_src3=cv2.imread('OKPictures//16_51_22.jpg')
+ng_src1=cv2.imread('NGPictures//16_16_08.jpg')
+ng_src2=cv2.imread('NGPictures//16_16_11.jpg')
+ng_src3=cv2.imread('NGPictures//16_16_57.jpg')
+imgData_list=[ok_src1,ok_src2,ok_src3,ng_src1,ng_src2,ng_src3]
+name_list=['ok1','ok2','ok3','ng1','ng2','ng3']
+imgFullInfo_list=[]
+for i in range(len(name_list)):
+    imgData_list[i]=cv2.cvtColor(imgData_list[i],cv2.COLOR_RGB2GRAY)
+    imgFullInfo_list.append([name_list[i],imgData_list[i]])
+
+drawHist(imgFullInfo_list)
+
