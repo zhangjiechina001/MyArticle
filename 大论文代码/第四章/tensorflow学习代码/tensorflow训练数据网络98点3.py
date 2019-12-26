051b47179008d369f4f2be11749815922434991f
Administrator <295737015@qq.com>
2019-12-26 18:06:35 +0800

    gw504

--- 大论文代码/第四章/tensorflow学习代码/tensorflow训练数据网络98点3.py
+++ 大论文代码/第四章/tensorflow学习代码/tensorflow训练数据网络98点3.py
@@ -25,7 +25,7 @@ def inference(input_tensor,avg_class,w1,b1,w2,b2):
         return tf.nn.relu(tf.matmul(layers1,w2)+b2)
     else:
         layers1=tf.nn.relu(tf.matmul(input_tensor,avg_class.average(w1))+avg_class.average(b1))
-        return tf.nn.relu(tf.matmul(layers1,avg_class.average(w2))+avg_class.average(b2))
+        return tf.nn.relu(tf.matmul(layers1,avg_class.average(w2))+avg_class.average(b2),name='prediction')
 
 def train(mnist):
     x=tf.placeholder(tf.float32,[None,INPUT_NODE],name='x-input')
@@ -84,10 +84,54 @@ def train(mnist):
 
         test_acc=sess.run(accuracy,feed_dict=test_feed)
         print('teain end! after {0} trains,validation accuracy use average model is {1}'.format(str(TRAIN_STEPS),str(test_acc)))
+        saver=tf.train.Saver()
+        saver.save(sess,'model_full_connection\\moduel_full_connection.ckpt',global_step=global_step)
+
+import cv2
+def detect(img,mnist):
+    # img=img[10:90,20:80]
+    img=cv2.resize(img,(28,28))
+    img=255-img[:,:]
+    # kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
+    # img=cv2.dilate(img,kernel)
+    img=(img[:,:]/255).astype('float32')
+    cv2.imshow('image',img)
+    cv2.waitKey()
+    img=img.reshape(1,28*28)
+    with tf.Session() as sess:
+        # 载入meta文件
+        saver = tf.train.import_meta_graph(r'E:\大论文\大论文代码\第四章\tensorflow学习代码\model_full_connection\moduel_full_connection.ckpt-30000.meta')
+        # 载入最近的一次保存的模型文件
+        saver.restore(sess, tf.train.latest_checkpoint(r"E:\大论文\大论文代码\第四章\tensorflow学习代码\model_full_connection/"))
+        # 建立图
+        graph = tf.get_default_graph()
+        # 初始化所有的变量
+        sess.run(tf.global_variables_initializer())
+        # 获取网络中的变量
+        X = graph.get_tensor_by_name('x-input:0')
+        # keep_prob = graph.get_tensor_by_name('keep_prob:0')
+        result = graph.get_tensor_by_name('prediction:0')
+        # 这上面的‘data’、‘keep_prob’、‘prediction’都是我们在训练时定义的变量名称
+        prediction=sess.run(result, feed_dict={X: img})
+        print(sess.run(tf.argmax(prediction,1)))
+        import matplotlib.pyplot as plt
+        # 可视化样本，下面是输出了训练集中前20个样本
+        fig, ax = plt.subplots(nrows=4, ncols=5, sharex='all', sharey='all')
+        ax = ax.flatten()
+        for i in range(20):
+            img = mnist.test.images[i].reshape(28, 28)
+            ax[i].imshow(img, cmap='gray')
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        plt.tight_layout()
+        plt.show()
+
 
 def main(argv=None):
     mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
-    train(mnist)
+    # train(mnist)
+    img=cv2.imread('formatedImg\\0.jpg',cv2.IMREAD_GRAYSCALE)
+    detect(img,mnist)
 
 if __name__=='__main__':
     main()
