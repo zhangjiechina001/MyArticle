bd331cbad9555b3933a58735e5f5b256b164c125
Administrator <295737015@qq.com>
2020-01-17 11:38:53 +0800

    gw504

--- 大论文代码/第五章/PyQt学习代码/UI文件/zhifangtu_fun.py
+++ 大论文代码/第五章/PyQt学习代码/UI文件/zhifangtu_fun.py
@@ -2,17 +2,63 @@ import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QSizePolicy, QFileDialog
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
 import cv2
+from matplotlib.figure import Figure
+
 from zhifangtu import Ui_Form
 
+#首先定义一个继承自FigureCanvas的类
+class Mydemo(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        plt.rcParams['font.family'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+        #创建一个2*2布局的表格
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes_1 = self.fig.add_subplot(2, 2, 1)
+        self.axes_2 = self.fig.add_subplot(2, 2, 2)
+        self.axes_3 = self.fig.add_subplot(2, 2, 3)
+        self.axes_4 = self.fig.add_subplot(2, 2, 4)
+        self.fig.tight_layout()
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self,QSizePolicy.Expanding,QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+import cv2
 class MainWindow(QtWidgets.QWidget,Ui_Form):
     def __init__(self):
         super(MainWindow,self).__init__()
         self.setupUi(self)
-        # self.widget_toolbar = NavigationToolbar(self.frame_4)
+        self.vlayout=QtWidgets.QVBoxLayout(self)
+        #创建图表的实例
+        self.cavas=Mydemo(width=5, height=4, dpi=100)
+        self.widget_toolbar=NavigationToolbar(self.cavas, self.widget)
+        self.v_Layout_dis.addWidget(self.widget_toolbar)
+        self.v_Layout_dis.addWidget(self.cavas)
+        self.btn_openfile_ywzf.clicked.connect(self.btn_open_file_ywzf_clicked)
+
+    def open_image_file(self):
+        '''打开一个图像文件'''
+        fileName, filetype = QFileDialog.getOpenFileName(self,
+                                                         "open file", '.',
+                                                         "jpg Files (*.jpg);;png Files (*.png);;All Files (*)")
+        return fileName
+
+
+    def btn_open_file_ywzf_clicked(self):
+        import numpy as np
+        file_name=self.open_image_file()
+        image=cv2.imread(filename=file_name,flags=cv2.IMREAD_GRAYSCALE)
+        #图片显示
+        self.cavas.axes_1.imshow(image,cmap='gray')
+        self.cavas.axes_1.set_title('原图')
+        self.cavas.axes_2.hist(image.ravel(),256,[0,255],color='r')
+        self.cavas.axes_2.set_title('直方图')
+        #在界面上显示
+        self.cavas.draw()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- 大论文代码/第五章/PyQt学习代码/gitChatdemo/chat/test.py
+++ 大论文代码/第五章/PyQt学习代码/gitChatdemo/chat/test.py
@@ -1,127 +1,129 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'DataDisplayUI.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
+# coding=utf8
 import sys
+import random
+from datetime import datetime
+import matplotlib, tushare
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QApplication
-from matplotlib.backends.backend_template import FigureCanvas
+import matplotlib.ticker as ticker
+
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib.lines import Line2D
+
+global real_data
+real_data = [20]
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.LineDisplayGB = QtWidgets.QGroupBox(self.centralwidget)
-        self.LineDisplayGB.setObjectName("LineDisplayGB")
-        self.gridLayout.addWidget(self.LineDisplayGB, 0, 0, 1, 1)
-        self.BarDisplayGB = QtWidgets.QGroupBox(self.centralwidget)
-        self.BarDisplayGB.setObjectName("BarDisplayGB")
-        self.gridLayout.addWidget(self.BarDisplayGB, 0, 1, 1, 1)
-        self.ImageDisplayGB = QtWidgets.QGroupBox(self.centralwidget)
-        self.ImageDisplayGB.setObjectName("ImageDisplayGB")
-        self.gridLayout.addWidget(self.ImageDisplayGB, 1, 0, 1, 1)
-        self.SurfaceDisplayGB = QtWidgets.QGroupBox(self.centralwidget)
-        self.SurfaceDisplayGB.setObjectName("SurfaceDisplayGB")
-        self.gridLayout.addWidget(self.SurfaceDisplayGB, 1, 1, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+class MyMplCanvas(FigureCanvas):
+    """这是一个窗口部件，即QWidget（当然也是FigureCanvasAgg）"""
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        fig, self.axes = plt.subplots()
+        self.compute_initial_figure()
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.LineDisplayGB.setTitle(_translate("MainWindow", "Line Display"))
-        self.BarDisplayGB.setTitle(_translate("MainWindow", "Bar Display"))
-        self.ImageDisplayGB.setTitle(_translate("MainWindow", "Image Display"))
-        self.SurfaceDisplayGB.setTitle(_translate("MainWindow", "3D Surface Display"))
+    def compute_initial_figure(self):
+        pass
 
-class Figure_Canvas(FigureCanvas):
-    def __init__(self,parent=None,width=3.9,height=2.7,dpi=100):
-        self.fig=Figure(figsize=(width,height),dpi=100)
-        super(Figure_Canvas,self).__init__(self.fig)
-        self.ax=self.fig.add_subplot(111)
-    def test(self):
-        x=[1,2,3,4,5,6,7]
-        y=[2,1,3,5,6,4,3]
-        self.ax.plot(x,y)
 
-import numpy as np
+class MyStaticMplCanvas(MyMplCanvas):
+    """静态画布"""
 
-class ImgDisp(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(ImgDisp, self).__init__(parent)
-        self.setupUi(self)
-        self.Init_Widgets()
+    def compute_initial_figure(self):
+        a = tushare.get_hist_data('000002', start='2017-05-22', end='2017-05-31')
+        t = a.open
+        s = [datetime.strptime(d, '%Y-%m-%d').date() for d in a.index]
+        self.axes.plot(s, t)
 
-    def Init_Widgets(self):
-        self.PrepareSamples()
-        self.PrepareLineCanvas()
-        self.PrepareBarCanvas()
-        self.PrepareImgCanvas()
-        self.PrepareSurfaceCanvas()
 
-    def PrepareSamples(self):
-        self.x = np.arange(-4, 4, 0.02)
-        self.y = np.arange(-4, 4, 0.02)
-        self.X, self.Y = np.meshgrid(self.x, self.y)
-        self.z = np.sin(self.x)
-        self.R = np.sqrt(self.X ** 2 + self.Y ** 2)
-        self.Z = np.sin(self.R)
+class MyDynamicMplCanvas(MyMplCanvas):
+    """动态画布：每1秒自动更新"""
 
-    def PrepareLineCanvas(self):
-        self.LineFigure = Figure_Canvas()
-        self.LineFigureLayout = QGridLayout(self.LineDisplayGB)
-        self.LineFigureLayout.addWidget(self.LineFigure)
-        self.LineFigure.ax.set_xlim(-4, 4)
-        self.LineFigure.ax.set_ylim(-1, 1)
-        self.line = Line2D(self.x, self.z)
-        self.LineFigure.ax.add_line(self.line)
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
 
-    def PrepareBarCanvas(self):
-        self.BarFigure = Figure_Canvas()
-        self.BarFigureLayout = QGridLayout(self.BarDisplayGB)
-        self.BarFigureLayout.addWidget(self.BarFigure)
-        self.BarFigure.ax.set_xlim(-4, 4)
-        self.BarFigure.ax.set_ylim(-1, 1)
-        self.bar = self.BarFigure.ax.bar(np.arange(-4, 4, 0.5), np.sin(np.arange(-4, 4, 0.5)), width=0.4)
-        self.patches = self.bar.patches
+    def compute_initial_figure(self):
+        self.axes.plot(real_data, 'r')
 
-    def PrepareImgCanvas(self):
-        self.ImgFigure = Figure_Canvas()
-        self.ImgFigureLayout = QGridLayout(self.ImageDisplayGB)
-        self.ImgFigureLayout.addWidget(self.ImgFigure)
-        self.ImgFig = self.ImgFigure.ax.imshow(self.Z, cmap='bone')
-        self.ImgFig.set_clim(-0.8, 0.8)
+    def update_figure(self):
+        global real_data
+        # 构建4个随机整数，位于闭区间[0, 10]
+        real_data.append(tushare.get_realtime_quotes('000581').price[0])
+        self.axes.set_xlim(0, 3600)
 
-    def PrepareSurfaceCanvas(self):
-        self.SurfFigure = Figure_Canvas()
-        self.SurfFigureLayout = QGridLayout(self.SurfaceDisplayGB)
-        self.SurfFigureLayout.addWidget(self.SurfFigure)
-        self.SurfFigure.ax.remove()
-        self.ax3d = self.SurfFigure.fig.gca(projection='3d')
-        self.Surf = self.ax3d.plot_surface(self.X, self.Y, self.Z, cmap='rainbow')
+        self.axes.plot(real_data, 'r')
+        self.draw()
+
+
+class ApplicationWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("程序主窗口")
+
+        self.file_menu = QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QWidget(self)
+
+        l = QVBoxLayout(self.main_widget)
+        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        l.addWidget(sc)
+        l.addWidget(dc)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+        # 状态条显示2秒
+        self.statusBar().showMessage("matplotlib 万岁!", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QMessageBox.about(self, "About",
+                          """embedding_in_qt5.py example
+                          Copyright 2015 BoxControL
+                          This program is a simple example of a Qt5 application embedding matplotlib
+                          canvases. It is base on example from matplolib documentation, and initially was
+                          developed from Florent Rougon and Darren Dale.
+                          http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+                          It may be used and modified with no restriction; raw copies as well as
+                          modified versions may be distributed without limitation.
+                          """
+                          )
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = ImgDisp()
-    form.show()
-    sys.exit(app.exec_())
+
+    aw = ApplicationWindow()
+    aw.setWindowTitle("PyQt5 与 Matplotlib 例子")
+    aw.show()
+    # sys.exit(qApp.exec_())
+    app.exec_()
