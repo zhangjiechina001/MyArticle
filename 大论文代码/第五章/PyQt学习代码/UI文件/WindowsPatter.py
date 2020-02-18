ae8625369c871f373f2924ec28ce3d860c4dde2b
Administrator <295737015@qq.com>
2020-02-18 15:41:28 +0800

    win10

---
+++ 大论文代码/第五章/PyQt学习代码/UI文件/WindowsPatter.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import QTimer, QDateTime, QUrl
+from PyQt5.QtGui import QColor, QBrush
+from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtWebEngineWidgets import *
+from functools import partial
+#设置不同的窗口风格 ['windowsvista', 'Windows', 'Fusion']
+class WindowsStyleDemo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('设置窗口的样式')
+
+        self.resize(500,300)
+        self.setWindowFlags(
+            QtCore.Qt.WindowMaximizeButtonHint | QtCore.Qt.WindowStaysOnTopHint)
+        # self.setWindowFlags(QtCore.Qt.WindowMaximizeButtonHint|QtCore.Qt.WindowStaysOnTopHint|QtCore.Qt.FramelessWindowHint)
+        self.setObjectName('mian_window')
+        self.setStyleSheet("#main_window{border-image:url(img_python.jpg);}")
+
+    def handleStyleChanged(self,style):
+        QApplication.setStyle(style)
+        # self.styleComBox.setCurrentIndex(1)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = WindowsStyleDemo()
+    main.show()
+    sys.exit(app.exec_())
